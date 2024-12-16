26fb7602405ce6b2550bb5b0af614ea482c34585
victrix17 <tupu@xtec.at>
2024-12-16 09:10:09 +0100

    MY_CMP AFEGIT

--- practica.c
+++ practica.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
@@ -7,6 +8,7 @@
 #define BUFFER_SIZE 1
 
 int main(int argc, char *argv[]) {
+//FUNCIO MY_TEE    
     // Comprovar arguments
     if (argc != 2) {
         printf(stderr, "Ús: %s <nom_arxiu>\n", argv[0]);
@@ -53,4 +55,25 @@ int main(int argc, char *argv[]) {
     }
 
     return EXIT_SUCCESS;
+
+
+//FUNCIO MY_CMP
+    char arxiu1 = argv[1];
+    char arxiu2 = argv[2];
+    int descripcio_arxiu1 = open(arxiu1, O_RDONLY);
+    int descripcio_arxiu2 = open(arxiu2, O_RDONLY);
+    if (arxiu1 == -1 || arxiu2 == -1) {
+        printf ("error en obrir l'arxiu")
+        
+    }
+    else {
+    while (read(descrpicio_arxiu1, , size_t count) < 0;){
+        int lectura_arxiu1 = read(descrpicio_arxiu1, , size_t count)
+        int lectura_arxiu2 = read(descrpicio_arxiu2, , size_t count)
+        //cada cop que hi ha un return linies +=1
+        //s'ha de tenir en compte que passa quan un arxiu s'acaba i l'altre no
+        //s'ha de pensar bé que retorna en cada cas (bytes iguals / bytes diferents)
+
+    }
+    }
 }
