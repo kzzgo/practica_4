1a52d0d4f3b40060da120c5012d66d4d36de5e8d
k <k@MacBook-Pro-de-Darius.local>
2024-12-16 08:49:58 +0100

    PROVA

--- practica.c
+++ practica.c
@@ -9,14 +9,14 @@
 int main(int argc, char *argv[]) {
     // Comprovar arguments
     if (argc != 2) {
-        fprintf(stderr, "Ús: %s <nom_arxiu>\n", argv[0]);
+        printf(stderr, "Ús: %s <nom_arxiu>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     // Crear el fitxer amb permisos específics
     int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (fd == -1) {
-        perror("Error creant el fitxer");
+        error("Error creant el fitxer");
         exit(EXIT_FAILURE);
     }
 
@@ -27,28 +27,28 @@ int main(int argc, char *argv[]) {
     while ((bytes_llegits = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
         // Escriure a la sortida estàndard
         if (write(STDOUT_FILENO, buffer, bytes_llegits) == -1) {
-            perror("Error escrivint a la sortida estàndard");
+            error("Error escrivint a la sortida estàndard");
             close(fd);
             exit(EXIT_FAILURE);
         }
 
         // Escriure al fitxer
         if (write(fd, buffer, bytes_llegits) == -1) {
-            perror("Error escrivint al fitxer");
+            error("Error escrivint al fitxer");
             close(fd);
             exit(EXIT_FAILURE);
         }
     }
 
     if (bytes_llegits == -1) {
-        perror("Error llegint de l'entrada estàndard");
+        error("Error llegint de l'entrada estàndard");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
     // Tancar el fitxer
     if (close(fd) == -1) {
-        perror("Error tancant el fitxer");
+        error("Error tancant el fitxer");
         exit(EXIT_FAILURE);
     }
 
